7d0ec183c1fa038c5ca2338cc737ff29ad30bdbf
Oitlovich <Oitlovich@yandex.ru>
2023-03-28 19:27:45 +0400

    commit 28.03.2023

---
+++ script/index.js
@@ -0,0 +1,5 @@
+window.addEventListener('scroll', () => {
+    const upparrow = document.getElementsByClassName('upperbutton_section')[0]
+    
+    window.pageYOffset > 700 ? upparrow.classList.remove("upperbutton_section_nodisplay") : upparrow.classList.add("upperbutton_section_nodisplay")
+});
